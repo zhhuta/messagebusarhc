9658d6fbf6af1aaaec500352b0b75eea78e5dc18
Vitaliy Zhhuta <zhhuta@gmail.com>
2018-04-18 22:00:39 +0300

    add EventDB inteface

--- structures.go
+++ structures.go
@@ -1,7 +1,20 @@
 package messagebusarhc
 
+// Event base object that we going to pass thought all our services.
 type Event struct {
-	Id      int64
+	ID      int64
 	Name    string
 	Message string
+}
+
+// Listing methods of EventDatabase
+type EventDatabase interface {
+	//get all events
+	ListEvents() ([]*Event, error)
+	//get aprticular event
+	GetEvent(id int64) (*Event, error)
+	//add event to the db
+	AddEevent(e Event) (id int64, err error)
+	// Close connection to db
+	Close()
 }
--- worker_db.go
+++ worker_db.go
@@ -3,15 +3,15 @@ package messagebusarhc
 import (
 	"database/sql"
 	"database/sql/driver"
-	"errors"
 	"fmt"
+
 	"github.com/go-sql-driver/mysql"
 )
 
 var createTableStatements = []string{
 	`CREATE DATABASES IF NOT EXISTS eventsarray EFAULT CHARACTER SET = 'utf8' DEFAULT COLLATE 'utf8_general_ci';`,
 	`USE eventsarray;`,
-	`CREATE TABLE IF NOT EXISTS events (
+	`CREATE TABL IF NOT EXISTS events (
 		id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 		name VARCHAR(255) NULL,
 		message VARCHAR(255) NULL,
@@ -20,27 +20,31 @@ var createTableStatements = []string{
 }
 
 type mysqlDB struct {
-	conn *sql.Stmt
-	list *sql.Stmt
+	conn *sql.DB
+
+	list   *sql.Stmt
 	insert *sql.Stmt
-	get *sql.Stmt
+	get    *sql.Stmt
 	update *sql.Stmt
 	delete *sql.Stmt
 }
 
-var _ EventsDatabase = &mysqlDB
+// Mapping EventDatabase to mysqlSDB
+var _ EventsDatabase = &mysqlDB{}
 
+//Structure that keeps mysql conneciton configuration
 type MySQLConfig struct {
-	Username, Password string
-	Host string
-	Port int
+	Username   string
+	Password   string
+	Host       string
+	Port       int
 	UnixSocket string
 }
 
 func (c MySQLConfig) dataStoreName(databasesName string) string {
 	var credentials string
 	if c.Username != "" {
-		credentials c.Username
+		credentials = c.Username
 		if c.Password != "" {
 			credentials = credentials + c.Password
 		}
@@ -53,7 +57,7 @@ func (c MySQLConfig) dataStoreName(databasesName string) string {
 }
 
 func (config MySQLConfig) ensureTableExists() error {
-	connection, err := sql.Open("mysql",config.dataStoreName(""))
+	connection, err := sql.Open("mysql", config.dataStoreName(""))
 	if err != nil {
 		return fmt.Errorf("mysql: could not get a connection: %v", err)
 	}
@@ -61,7 +65,7 @@ func (config MySQLConfig) ensureTableExists() error {
 
 	if connection.Ping() == driver.ErrBadConn {
 		return fmt.Errorf("mysql: couldnt connect to the database." +
-		"may be bad address, or this address is not whitelisted for access.")
+			"may be bad address, or this address is not whitelisted for access.")
 	}
 	// TODO: this is hardcord - review it later
 	if _, err := connection.Exec("USE eventsarray"); err != nil {
@@ -69,6 +73,16 @@ func (config MySQLConfig) ensureTableExists() error {
 			return createTable(connection)
 		}
 	}
+
+	if _, err := connection.Exec("DESCRIBE events"); err != nil {
+		// MySQL error 1146 is "table does not exist"
+		if mErr, ok := err.(*mysql.MySQLError); ok && mErr.Number == 1146 {
+			return createTable(connection)
+		}
+		// Unknown error.
+		return fmt.Errorf("mysql: could not connect to the database: %v", err)
+	}
+	return nil
 
 }
 
@@ -84,5 +98,117 @@ func createTable(conn *sql.DB) error {
 
 func createNewMySQLDB(config MySQLConfig) (EventsDatabase, error) {
 	//Check if DB and tables are present
-	
+	if err := config.ensureTableExists(); err != nil {
+		return nil, err
+	}
+
+	conn, err := sql.Open("mysql", config.dataStoreName("eventsarray"))
+	if err != nil {
+		return nil, fmt.Errorf("mysql: could not get a connection: %v", err)
+	}
+	if err := conn.Ping(); err != nil {
+		conn.Close()
+		return nil, fmt.Errorf("mysql: could not establish a good connection: %v", err)
+	}
+
+	db := &mysqlDB{
+		conn: conn,
+	}
+	if db.list, err = conn.Prepare(listStatement); err != nil {
+		return nil, fmt.Errorf("mysql: prepare list: %v", err)
+	}
+
+	if db.get, err = conn.Prepare(getStatement); err != nil {
+		return nil, fmt.Errorf("mysql: prepare get: %v", err)
+	}
+	if db.insert, err = conn.Prepare(insertStatement); err != nil {
+		return nil, fmt.Errorf("mysql: prepare insert: %v", err)
+	}
+	if db.update, err = conn.Prepare(updateStatement); err != nil {
+		return nil, fmt.Errorf("mysql: prepare update: %v", err)
+	}
+	if db.delete, err = conn.Prepare(deleteStatement); err != nil {
+		return nil, fmt.Errorf("mysql: prepare delete: %v", err)
+	}
+
+	return db, nil
 }
+
+func (db *mysqlDB) Close() {
+	db.conn.Close()
+}
+
+// rowScanner is implemented by sql.Row and sql.Rows
+type rowScanner interface {
+	Scan(dest ...interface{}) error
+}
+
+// scanBook reads a book from a sql.Row or sql.Rows
+func scanEvent(s rowScanner) (*Event, error) {
+	var (
+		id      int64
+		name    sql.NullString
+		message sql.NullString
+	)
+	if err := s.Scan(&id, &name, &message); err != nil {
+		return nil, err
+	}
+
+	book := &Event{
+		ID:      id,
+		Name:    name.String,
+		Message: message.String,
+	}
+	return book, nil
+}
+
+// method to Get all records from DB ordered by name
+const listStatement = `SELECT * FROM events ORDER BY name`
+
+func (db *mysqlDB) ListEvents() ([]*Event, error) {
+	rows, err := db.list.Query()
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []*Event
+	for rows.Next() {
+		event, err := scanEvent(rows)
+		if err != nil {
+			return nil, fmt.Errorf("mysql: could not read row: %v", err)
+		}
+
+		events = append(events, event)
+	}
+
+	return events, nil
+}
+
+// Method to Get a specific event by id.
+const getStatement = "SELECT * FROM books WHERE id = ?"
+
+func (db *mysqlDB) GetEvent(id int64) (*Event, error) {
+	event, err := scanEvent(db.get.QueryRow(id))
+	if err == sql.ErrNoRows {
+		return nil, fmt.Errorf("mysql: could not find event with id %d", id)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("mysql: could not get event: %v", err)
+	}
+	return event, nil
+}
+
+const insertStatement = `
+ INSERT INTO events (
+   name, message, 
+ ) VALUES (?, ?)`
+
+func (db *mysqlDB) AddEevent(e Event) (id int64, err error) {
+	// emplement  insert.
+	return 0, nil
+}
+
+const updateStatement = ``
+
+const deleteStatement = ``
